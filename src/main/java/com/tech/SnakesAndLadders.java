31d4cd4be77e1f1d7f3ebe74fff18582fca4487a
Aviral Nimbekar <aviralnimbekar12@gmail.com>
2025-08-09 14:42:19 +0530

    initial commit

---
+++ src/main/java/com/tech/SnakesAndLadders.java
@@ -0,0 +1,7 @@
+package com.tech;
+
+public class SnakesAndLadders {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snakes and Ladders game!!!");
+    }
+}
