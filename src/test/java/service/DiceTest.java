ed15e221594fd2598c767cefe0752a3b82a963ac
Aviral Nimbekar <aviralnimbekar12@gmail.com>
2025-08-09 15:04:56 +0530

    Add: Dice logic

---
+++ src/test/java/service/DiceTest.java
@@ -0,0 +1,36 @@
+package service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DiceTest {
+
+    @Test
+    void shouldNeverRollADiceWithZero() {
+        Dice dice = new Dice();
+
+        int actualRoll = dice.roll();
+
+        assertNotEquals(0, actualRoll);
+    }
+
+    @Test
+    void shouldGenerateARandomNumberBetweenOneToSix() {
+        Dice dice = new Dice();
+
+        int actualRoll = dice.roll();
+
+        assertTrue(1 <= actualRoll  && actualRoll <= 6);
+    }
+
+    @Test
+    void shouldNotGenerateARandomNumberOtherThanOneToSix() {
+        Dice dice = new Dice();
+
+        int actualRoll = dice.roll();
+
+        assertFalse(actualRoll < 1 && 6 < actualRoll );
+    }
+}
