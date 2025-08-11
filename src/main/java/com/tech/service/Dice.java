cc588a12f91300b149418d9051ea3b6385ffc87d
Aviral Nimbekar <aviralnimbekar12@gmail.com>
2025-08-11 12:16:46 +0530

    Add: Print statement in dice

--- src/main/java/com/tech/service/Dice.java
+++ src/main/java/com/tech/service/Dice.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Dice {
 
     private static final int MIN_DICE_NUMBER = 1;
-    private static final int MAX_DICE_NUMBER = 7;
+    private static final int MAX_DICE_NUMBER = 7; // Exclusive upper bound
     private final Random random;
 
     public Dice() {
@@ -13,7 +13,9 @@ public class Dice {
     }
 
     public int roll() {
-        return random.nextInt(MIN_DICE_NUMBER, MAX_DICE_NUMBER);
+        int diceOutcome = random.nextInt(MIN_DICE_NUMBER, MAX_DICE_NUMBER);
+        System.out.println("Dice roll :: " + diceOutcome);
+        return diceOutcome;
     }
 
 }
