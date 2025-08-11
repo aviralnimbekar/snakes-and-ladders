d9e2436115a4ccd4d850aa5b684a1ec6bfdfd2fc
Aviral Nimbekar <aviralnimbekar12@gmail.com>
2025-08-11 12:17:07 +0530

    Add: main method to start a game

--- src/main/java/com/tech/SnakesAndLadders.java
+++ src/main/java/com/tech/SnakesAndLadders.java
@@ -1,7 +1,14 @@
 package com.tech;
 
+import com.tech.service.Game;
+
 public class SnakesAndLadders {
     public static void main(String[] args) {
+        System.out.println("-----------------------------------------");
         System.out.println("Welcome to the Snakes and Ladders game!!!");
+        System.out.println("-----------------------------------------\n");
+        Game game = new Game();
+        game.start();
+
     }
 }
